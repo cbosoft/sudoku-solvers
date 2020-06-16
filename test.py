d2d33375fb03e54bce040caebe2baa34c8980e63
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-16 09:59:15 +0100

    Hardcode initial test sudoku puzzle

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
 import sys
 
-from sudopy import generate_puzzle, solve_puzzle
+from sudopy import Puzzle
+
+# http://lipas.uwasa.fi/~timan/sudoku/
+puzzles = [
+    [0, 4, 0, 0, 0, 0, 1, 7, 9],
+    [0, 0, 2, 0, 0, 8, 0, 5, 4],
+    [0, 0, 6, 0, 0, 5, 0, 0, 8],
+    [0, 8, 0, 0, 7, 0, 9, 1, 0],
+    [0, 5, 0, 0, 9, 0, 0, 3, 0],
+    [0, 1, 9, 0, 6, 0, 0, 4, 0],
+    [3, 0, 0, 4, 0, 0, 7, 0, 0],
+    [5, 7, 0, 1, 0, 0, 2, 0, 0],
+    [9, 2, 8, 0, 0, 0, 0, 6, 0]
+]
 
 def test():
-    puzzle = generate_puzzle()
-    solved = solve_puzzle(puzzle)
 
-    if puzzle.complete():
-        print('pass')
-    else:
-        print('fail')
-        sys.exit(1)
+    for puzzle in puzzles:
+        puzzle = Puzzle(puzzles[0])
+        solved = puzzle.solve()
+
+        if puzzle.complete():
+            print('pass')
+        else:
+            print('fail')
+            sys.exit(1)
