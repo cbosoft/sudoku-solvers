ee514acb2498764064425c69c0f6c73bd073606d
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-06-16 10:22:43 +0100

    Add test for completion check of puzzle

--- test.py
+++ test.py
@@ -15,6 +15,36 @@ puzzles = [
     [9, 2, 8, 0, 0, 0, 0, 6, 0]
 ]
 
+solutions = [
+    [8, 4, 5, 6, 3, 2, 1, 7, 9],
+    [7, 3, 2, 9, 1, 8, 6, 5, 4],
+    [1, 9, 6, 7, 4, 5, 3, 2, 8],
+    [6, 8, 3, 5, 7, 4, 9, 1, 2],
+    [4, 5, 7, 2, 9, 1, 8, 3, 6],
+    [2, 1, 9, 8, 6, 3, 5, 4, 7],
+    [3, 6, 1, 4, 2, 9, 7, 8, 5],
+    [5, 7, 4, 1, 8, 6, 2, 9, 3],
+    [9, 2, 8, 3, 5, 7, 4, 6, 1]
+]
+
+def test_completion_check():
+    for puzzle in puzzles:
+        puzzle = Puzzle(puzzle)
+        if puzzle.complete():
+            print('fail: incomplete puzzle marked as complete')
+            exit(1)
+        else:
+            print('pass')
+
+    for puzzle in solutions:
+        solution = Puzzle(solution)
+        if solution.complete():
+            print('pass')
+        else:
+            print('fail: complete puzzle marked as incomplete')
+            exit(1)
+
+
 def test():
 
     for puzzle in puzzles:
